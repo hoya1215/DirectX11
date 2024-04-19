e816c5715a4349745592c4759da774ac5dd20154
hoya1215 <157784319+hoya1215@users.noreply.github.com>
2024-04-19 09:38:21 +0900

    Add files via upload

---
+++ Resources/GeometryGenerator.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+#include "Headers.h"
+
+#include "AnimationClip.h"
+#include "MeshData.h"
+#include "Vertex.h"
+
+
+
+class GeometryGenerator {
+  public:
+
+    static vector<MeshData> ReadFromFile(std::string basePath,
+                                         std::string filename,
+                                         bool revertNormals = false);
+
+    static auto ReadAnimationFromFile(string basePath, string filename,
+                                      bool revertNormals = false)
+        -> tuple<vector<MeshData>, AnimationData>;
+
+    static void Normalize(const Vector3 center, const float longestLength,
+                          vector<MeshData> &meshes, AnimationData &aniData);
+
+    static MeshData MakeSquare(const float scale = 1.0f,
+                               const Vector2 texScale = Vector2(1.0f));
+
+    static MeshData MakeCircle(const float scale = 1.0f,
+                               const Vector2 texScale = Vector2(1.0f));
+    static MeshData MakeSquareGrid(const int numSlices, const int numStacks,
+                                   const float scale = 1.0f,
+                                   const Vector2 texScale = Vector2(1.0f));
+
+    static MeshData MakeBridge(const int numStacks, const float scale = 1.0f,
+                               const Vector2 texScale = Vector2(1.0f));
+
+    static MeshData MakeBox(const float scale = 1.0f);
+    static MeshData MakeCylinder(const float bottomRadius,
+                                 const float topRadius, float height,
+                                 int numSlices);
+    static MeshData MakeSphere(const float radius, const int numSlices,
+                               const int numStacks,
+                               const Vector2 texScale = Vector2(1.0f));
+
+    static MeshData MakeWireBBox(const Vector3 center, const Vector3 extent);
+
+    static MeshData MakeGrass();
+    static MeshData MakeTetrahedron();
+    static MeshData MakeIcosahedron();
+    static MeshData SubdivideToSphere(const float radius, MeshData meshData);
+};
+
---
+++ Resources/Vertex.h
@@ -0,0 +1,68 @@
+﻿#pragma once
+
+#include "Headers.h"
+
+
+
+
+
+struct Vertex {
+    Vector3 position;
+    Vector3 normalModel;
+    Vector2 texcoord;
+    Vector3 tangentModel;
+    //Vector3 biTangentModel;
+    // biTangent는 쉐이더에서 계산
+};
+
+struct SkinnedVertex {
+    Vector3 position;
+    Vector3 normalModel;
+    Vector2 texcoord;
+    Vector3 tangentModel;
+
+    float blendWeights[8] = {0.0f, 0.0f, 0.0f, 0.0f,
+                             0.0f, 0.0f, 0.0f, 0.0f};  // BLENDWEIGHT0 and 1
+    uint8_t boneIndices[8] = {0, 0, 0, 0, 0, 0, 0, 0}; // BLENDINDICES0 and 1
+
+
+};
+
+struct VertexIndex {
+    Vector3 position;
+    Vector3 normalModel;
+    Vector3 texcoord;
+    Vector3 tangentModel;
+    //float index;
+    // Vector3 biTangentModel;
+    //  biTangent는 쉐이더에서 계산
+};
+
+
+struct GrassVertex {
+    Vector3 posModel;
+    Vector3 normalModel;
+    Vector2 texcoord;
+
+};
+
+
+
+struct GrassInstance {
+    Matrix instanceWorld;
+    float windStrength;
+};
+
+struct SnowVertex
+{
+    Vector3 posModel;
+};
+
+
+struct SnowInstance {
+    Matrix instanceWorld;
+    float velocity;
+};
+
+
+
